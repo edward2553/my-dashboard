e4fd180648ed93134b8c9a61702f9826a01ff849
Edward Andres <emoron@abatechnology.com>
2024-05-08 20:48:04 -0500

    section 4 finished

---
+++ app/dashboard/counter/page.tsx
@@ -0,0 +1,20 @@
+
+import { CarCounter } from '@/app/shopping-cart';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Counter',
+  description: 'Contador',
+};
+
+const CounterPage = () => {
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span className='mb-2'>Productos en el carrito</span>
+      <CarCounter />
+    </div>
+  );
+};
+
+export default CounterPage;
